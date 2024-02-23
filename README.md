b07b41689fcb3536ace83f143289f6293e331721
Feros47 <Niklasm47@live.dk>
2024-02-23 16:50:31 +0100

    Youtube works

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 
  It will upload a userspecified amount of videos to each platform and thereafter move the videos to a different userspecified directory 
 
-So far Reels works.
+So far Reels and Youtube Shorts works!
 
 Missing TikTok Api... Will continue with html parsing/scraping
-
-Haven't started on Youtube yet, but should be pretty easy since the api is public
--- UploadPosts.py
+++ UploadPosts.py
@@ -1,42 +1,27 @@
 from instagrapi import Client
 from FindPosts import createPostDict
+from YoutubeUpload import main as youtube_upload
 import os
 import time
 
 cl = Client()
-cl.login("Lucidityy4", "Cdu79wqs.123")
-
+cl.login("Lucidityy4", "Niklasm34")
 
 def uploadInstaPosts(listDisct):
     reelsDict = listDisct
     for post in reelsDict:
         file_path = post['path']
-        # Assuming you want to include tags as hashtags in the caption
         caption = post['description'] + ' '.join(['#' + "tag "])  # Adjust tags as needed
         # Call clip_upload with the correct parameters
         cl.clip_upload(file_path, caption)
-    time.sleep(10)
-    movePosts(reelsDict['path'], "posted")
 
-from instagrapi import Client
-from FindPosts import createPostDict
-import os
-import time
-
-cl = Client()
-cl.login("Lucidityy4", "Cdu79wqs.123")
-
-
-def uploadInstaPosts(listDisct):
-    reelsDict = listDisct
-    for post in reelsDict:
+def uploadYoutubePosts(listDict):
+    shortsDict = listDict
+    for post in shortsDict:
         file_path = post['path']
-        # Assuming you want to include tags as hashtags in the caption
-        caption = post['description'] + ' '.join(['#' + "tag "])  # Adjust tags as needed
-        # Call clip_upload with the correct parameters
-        cl.clip_upload(file_path, caption)
-    time.sleep(3)
-    movePosts(reelsDict, "posted")
+        caption = post['description']# + ' '.join(['#' + "tag "])  # Adjust tags as needed
+        # Call youtube_upload with the correct parameters
+        youtube_upload(file_path, caption)
 
 def movePosts(listDict, directory):
     for post in listDict:
@@ -51,5 +36,8 @@ def movePosts(listDict, directory):
 def uploadPosts(directory, listLength):
     listDict = createPostDict(directory, listLength)
     uploadInstaPosts(listDict)
+    uploadYoutubePosts(listDict)
+    time.sleep(20)
+    movePosts(listDict, "posted")
 
 uploadPosts(".", 1)
